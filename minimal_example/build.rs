ef5340947e946fc53a8a1353500933e3c7da9e76
Victor Nordam Suadicani <victor.suadicani@issuu.com>
2022-06-23 12:38:43 +0200

    Remove derive_from_error

--- minimal_example/build.rs
+++ minimal_example/build.rs
@@ -1,17 +1,16 @@
-use kanin::derive::ProstDeriveExt;
-
 fn main() {
     prost_build::Config::new()
         .out_dir("src/protobuf/generated")
         // We need to derive FromError on any type that we want to use
         // as a return type from a handler. So we just list them here.
         // These are paths into the .proto file, not the generated Rust code.
-        .derive_from_error(&[
-            "InvalidRequest",
-            "InternalError",
-            "EchoResponse",
+        .type_attribute("InvalidRequest", "#[derive(kanin::derive::FromError)]")
+        .type_attribute("InternalError", "#[derive(kanin::derive::FromError)]")
+        .type_attribute("EchoResponse", "#[derive(kanin::derive::FromError)]")
+        .type_attribute(
             "EchoResponse.response",
-        ])
+            "#[derive(kanin::derive::FromError)]",
+        )
         .compile_protos(&["src/protobuf/echo.proto"], &["src"])
         .expect("Failed to compile .proto files")
 }
